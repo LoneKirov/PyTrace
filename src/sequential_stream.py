ee7e701c9a443b69517b20b7a3ad672fbfa3383f
Adam Miller <kirov.sama@gmail.com>
2012-04-02 15:55:11 -0700

    Dynamic attribute name

--- src/sequential_stream.py
+++ src/sequential_stream.py
@@ -1,19 +1,11 @@
 from itertools import takewhile, zip_longest, filterfalse
 
 class Detector(object):
-    def __init__(self, cmds, tDelta = 1000):
+    def __init__(self, cmds, attrName = 'stream', tDelta = 1000):
+        self.__attrName = attrName
         self.__tDelta = tDelta
         self.__cmds = cmds
         self.__nextStreamID = 0
-        pass
-
-    def __startStream(self, cmd):
-        pass
-
-    def __findStream(self, cmd):
-        pass
-
-    def __endStream(self, s):
         pass
 
     def __iter__(self):
@@ -24,7 +16,7 @@ class Detector(object):
         for cmd in self.__cmds:
             issued.append(cmd)
             def isExpired(x):
-                return (cmd.sTime() - x.eTime() > self.__tDelta * 1000) or hasattr(cmd, 'stream')
+                return (cmd.sTime() - x.eTime() > self.__tDelta * 1000) or hasattr(cmd, self.__attrName)
             for e in list(filter(isExpired, inFlight)):
                 inFlight.remove(e)
                 expired.append(e)
@@ -35,10 +27,11 @@ class Detector(object):
                 issued.remove(t[0])
                 yield t[0]
             for c in filter(lambda x: hasattr(x.start(), "sectorCount") and cmd.start().lba == x.start().lba + x.start().sectorCount, inFlight):
-                if hasattr(c, 'stream'):
-                    cmd.stream = c.stream
+                if hasattr(c, self.__attrName):
+                    setattr(cmd, self.__attrName, getattr(c, self.__attrName))
                 else:
-                    cmd.stream = c.stream = self.__nextStreamID
+                    setattr(c, self.__attrName, self.__nextStreamID)
+                    setattr(cmd, self.__attrName, getattr(c, self.__attrName))
                     self.__nextStreamID += 1
                 break
             inFlight.append(cmd)
